9ca6bece799cf034853d6c0852ffc02e72a55075
Alberto Rodriguez <alberto.rodriguez@springer.com>
2013-06-22 22:52:21 +0100

    Initial commit - Simple login with unit tests

---
+++ src/main/java/org/ardlema/model/User.java
@@ -0,0 +1,28 @@
+package org.ardlema.model;
+
+public class User {
+    private String userName;
+
+    private String password;
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
